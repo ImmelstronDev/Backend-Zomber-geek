749447d83396f4ecaa23556528d221b521192b0b
ImmelstronDev <wishandra.afk@gmail.com>
2023-02-26 12:45:54 +0300

    fix: return cors

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const app = express();
 app.use(express.json())
 app.use('/api/auth', router)
 app.use(cookieParser())
-// app.use(cors({credentials: true, origin:['https://deploy-preview-10--amazing-gaufre-1ebc58.netlify.app/', 'http://localhost:8080/', 'https://genuine-pastelito-0e12dc.netlify.app/']}))
+app.use(cors({credentials: true, origin:'*'}))
+
+// ['https://deploy-preview-10--amazing-gaufre-1ebc58.netlify.app/', 'http://localhost:8080/', 'https://genuine-pastelito-0e12dc.netlify.app/']
 
 // app.get('/', (req, res) => {
 //     console.log(req.query);
